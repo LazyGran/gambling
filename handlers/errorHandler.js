fc0513a47ef1feae84918a71b049ec3bd4bef3b9
LazyGran <xgraron@gmail.com>
2025-05-18 23:16:56 +0200

    added basic error handler that will be important later

---
+++ handlers/errorHandler.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder } = require("discord.js")
+const jsonfile	= require("jsonfile")
+const fs 		= require("fs")
+
+async function error(interaction, e_text)
+{
+	const embed = new EmbedBuilder()
+	.setTitle("An error has occured")
+	 .setColor(`#e80400`)
+    .setDescription(e_text)
+
+    try     {const reply        = await interaction.reply({embeds: [embed]})       }   //trying to reply normaly
+    catch   {const editReply    = await interaction.editReply({embeds: [embed]})   }   //if that fails, reply probably needs to be edited
+}
+
+module.exports =
+{
+    error,
+}
