81ddaefafdcbaf7a5fe87218508534478962a05c
LazyGran <xgraron@gmail.com>
2025-05-19 15:44:30 +0200

    Handler can now draw a card and burn x amount of cards

--- handlers/cardHandler.js
+++ handlers/cardHandler.js
@@ -1,7 +1,9 @@
 const { EmbedBuilder }  = require("discord.js")
+const { Random }        = require("random-js")
 const jsonfile	        = require("jsonfile")
 const fs                = require("fs")
 
+const random            = new Random()
 const deck_template     = [ "Ace", "Ace", "Ace", "Ace", 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, "Jack", "Jack", "Jack", "Jack", "Queen", "Queen", "Queen", "Queen", "King", "King", "King", "King"]
 
 //create a new deck of cards 
@@ -29,13 +31,48 @@ async function create(UID, size)
 //draw from an existing deck of cards
 async function draw(ID) 
 {
+    try 
+    { 
+        const deck  = jsonfile.readFileSync(`database/deck_${ID}`)
+        if (deck.length < 1) return{success: false, reason: "No more cards remaining in deck"}
 
+        const n     = random.integer(0, deck.length - 1)
+        const card  = deck[n]
+
+        deck.splice(n, 1)
+        jsonfile.writeFileSync(`database/deck_${ID}`, deck)
+
+        return{ success: true, card: card}
+    }
+    catch   
+    { 
+        return{ success: false, reason: "Error drawing card from deck" }
+    }
 }
 
 //burn card(s) from an existing deck
 async function burn(ID, amount)
 {
+    try 
+    { 
+        amount = amount || 1
+        for (let i = 0; i < amount; i++)
+        {
+            const deck  = jsonfile.readFileSync(`database/deck_${ID}`)
+            if (deck.length < 1) return{success: false, reason: "No more cards remaining in deck"}
 
+            const n     = random.integer(0, deck.length - 1)
+            const card  = deck[n]
+
+            deck.splice(n, 1)
+            jsonfile.writeFileSync(`database/deck_${ID}`, deck)
+        } 
+        return{ success: true}
+    }
+    catch   
+    { 
+        return{ success: false, reason: "Error burning card from deck" }
+    }
 }
 
 //delete a deck 
@@ -44,7 +81,6 @@ async function remove(ID)
     try { jsonfile.readFileSync(`database/deck_${ID}`) }
     catch   
     { 
-        reason = "Couldn't find deck"
         return{ success: false, reason: "Deck not found" }
     }
 
