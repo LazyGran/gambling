69abe2e33ea5853515f567ba48877387088bc55d
LazyGran <xgraron@gmail.com>
2025-05-20 17:40:29 +0200

    added information about public beta + bug report info

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -7,7 +7,8 @@ async function error(interaction, e_text)
 	const embed = new EmbedBuilder()
 	.setTitle("An error has occured")
 	.setColor(`#e80400`)
-    .setDescription(e_text)
+    .setDescription(e_text + "\n\n-# If you believe this is a bug, contact <@467019235328000001>")
+    .setFooter({ text: 'Please remember this is a public beta!' });
 
     try     { await interaction.reply({embeds: [embed]})       }   //trying to reply normaly
     catch   { await interaction.editReply({embeds: [embed]})   }   //if that fails, reply probably needs to be edited
