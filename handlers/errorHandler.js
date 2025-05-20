94c5a269ac5e4b84f760e6789c449cc74bf5f754
LazyGran <xgraron@gmail.com>
2025-05-21 00:37:35 +0200

    error handler now removes all components from the embed (error mid-game was resulting in an error embed with buttons)

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -1,17 +1,18 @@
-const { EmbedBuilder } 	= require("discord.js")
-const jsonfile			= require("jsonfile")
-const fs 				= require("fs")
+const { EmbedBuilder, ActionRowBuilder } 	= require("discord.js")
+const jsonfile  = require("jsonfile")
+const fs 		= require("fs")
 
 async function error(interaction, e_text)
 {
+    const row   = new ActionRowBuilder()
 	const embed = new EmbedBuilder()
 	.setTitle("An error has occured")
 	.setColor(`#e80400`)
     .setDescription(e_text + "\n\n-# If you believe this is a bug, contact <@467019235328000001>")
     .setFooter({ text: 'Please remember this is a public beta!' });
 
-    try     { await interaction.reply({embeds: [embed]})       }   //trying to reply normaly
-    catch   { await interaction.editReply({embeds: [embed]})   }   //if that fails, reply probably needs to be edited
+    try     { await interaction.reply({ embeds: [embed], components: [] })      }   //trying to reply normaly
+    catch   { await interaction.editReply({ embeds: [embed], components: [] })  }   //if that fails, reply probably needs to be edited
 }
 
 module.exports =
