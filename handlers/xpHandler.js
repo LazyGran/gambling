ae990fc15bfd7987e3e49fb23d76aadd091e8d47
LazyGran <xgraron@gmail.com>
2025-05-28 21:58:17 +0200

    r removed logging

--- handlers/xpHandler.js
+++ handlers/xpHandler.js
@@ -4,16 +4,12 @@ const dh 		= require("../handlers/dataHandler.js")
 
 function leveling(userStats, reward)
 {
-	console.log(userStats.xp, userStats.level, reward)
-
 	userStats.xp = userStats.xp + reward
-	const xpreq			= 20 * (userStats.level * userStats.level) - userStats.xp
-
-	console.log(userStats.xp, xpreq)
+	
+	const xpreq	= 20 * (userStats.level * userStats.level) - userStats.xp
 
 	if(xpreq <= 0) userStats.level ++;
 
-	console.log(userStats.xp, userStats.level)
 	dh.userSave(userStats)
 }
 
