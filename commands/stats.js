59b9341a116b6a1e50806e8f1a2dd0f70a4fed36
LazyGran <xgraron@gmail.com>
2025-05-28 22:37:37 +0200

    now shows level progress bar

--- commands/stats.js
+++ commands/stats.js
@@ -11,12 +11,28 @@ module.exports =
 		await interaction.deferReply()
 
         const { chips, level, xp, custom } 	= userStats
+        const xpreq                         = 20 * (level * level) 
+        const one_per                       = xpreq / 10
+        const progress                      = Math.floor(xp / one_per)
         const thumb							= custom.thumbnail || interaction.user.displayAvatarURL({ dynamic: true }) 
+
+        let bar       = "" 
+        let remaining = 10
+
+        for(i = 0; i < progress; i++)
+        {
+            remaining--
+            bar += "🟩"
+        }
+        for(i = 0; i < remaining; i++)
+        {
+            bar += "⬛"
+        }
         
         const embed = new EmbedBuilder()
         .setTitle(`${interaction.user.username}'s info:`)
         .setThumbnail(thumb)
-        .setDescription(`**Money:** ${chips}\n**Level:** ${level}`)
+        .setDescription(`**Money:** ${chips} \n**Level:** ${level} \n-# Next level: ${bar}`)
 
         interaction.editReply({ embeds: [embed] })
     }
