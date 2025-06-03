e423957d2103ea4e2c6c1928b6112a9a8ca93cd1
LazyGran <xgraron@gmail.com>
2025-06-03 16:58:03 +0200

    basic structure for roulette wheel is created & bot replies to the intereaction with an embed

---
+++ games/roulette.js
@@ -0,0 +1,34 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } 	= require("discord.js")
+const { Random }														= require("random-js")
+const dh 	= require("../handlers/dataHandler.js")
+const eh 	= require("../handlers/errorHandler.js")
+
+async function main(interaction, bet, userStats, UID, chosen)
+{
+	console.log(chosen)
+	const wheel = []
+
+	for(i = 0; i < 37; i++)
+	{
+		let type 	= ""
+
+		if 		(i === 0) 		type = "green"
+		else if (i % 2 === 0)	type = "black"
+		else 					type = "red"
+
+		wheel.push({ space: i, type: type })
+	}
+
+	const embed = new EmbedBuilder()
+	.setColor("#259dd9")
+	.setTitle("The wheel is spinning..")
+	.setThumbnail('https://media.discordapp.net/attachments/1242636042469642300/1376641025606549606/roulette_spin.gif?ex=68380a9d&is=6836b91d&hm=cb8533d5df401544391aa8aaa8e4e1c45ea70cea8b07d34d04c223cdf832c687&=')
+	.setDescription(`**Players:**`)
+
+	const initial	= await interaction.editReply({ embeds: [embed] })
+}
+
+module.exports =
+{
+    main,
+}
