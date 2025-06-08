85026f1cf0bf4c21791ee005db770455ae8f0f53
LazyGran <xgraron@gmail.com>
2025-06-08 20:26:19 +0200

    Moved dev command line tools to a seperate handler All logging must be done through here (dev.log instead of console.log) as to not fuck up those tools

---
+++ handlers/dev.js
@@ -0,0 +1,124 @@
+const readline  = require("readline");
+const fs        = require("fs")
+const path      = require("path")
+const dh        = require('../handlers/dataHandler.js')
+
+const rl = readline.createInterface(
+{
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '- '
+});
+    
+async function devTools()
+{
+    rl.prompt();
+
+    rl.on('line', (line) => 
+    {
+        const input     = line.trim();
+        const args      = input.split(" ")
+        const command   = args.shift()
+
+
+        switch (command) 
+        {
+            case "set":
+            {
+                const [ UID, key, value ] = args;
+                
+                if(!UID || !key || value === undefined)
+                {
+                    console.log("Usage: set <id> <property> <value> \nExample: \nset 467019235328000001 money 100")
+                    break;
+                }
+
+                let parsedVal;
+                if(value === "true")        parsedVal = true
+                else if(value === "false")  parsedVal = false
+                else if(!isNaN(value))      parsedVal = Number(value)
+                else                        parsedVal = value
+            
+                const userStats = dh.devGet(UID)
+
+                if (userStats === 0) 
+                {
+                    console.log("User not found!")
+                    break;
+                }
+                else if(!(key in userStats))
+                {
+                    console.log("Key not found!")
+                    break;
+                }
+
+                userStats[key] = parsedVal
+
+                dh.userSave(userStats)
+
+                console.log(`Successfully set ${userStats.userID}.${key} to ${userStats[key]}`)
+
+                break;
+            }
+
+            case "get":
+            {
+                const UID = args
+
+                if(!UID)
+                {
+                    console.log("Usage: get <id> \nExample: \nget 467019235328000001")
+                    break;
+                }
+
+                const userStats = dh.devGet(UID)
+
+                if (userStats === 0) 
+                {
+                    console.log("User not found!")
+                    break;
+                }
+
+                console.log(userStats)
+
+                break;
+            }
+
+        case "backup":
+        {
+            const date  = new Date().toISOString().split('T')[0]
+            const src   = path.join("database", "userdata.json")
+            const dest  = path.join("database", "backups", `backup_${date}.json`)
+
+            fs.copyFileSync(src, dest)
+
+            console.log("Successfully backed up Database " + date)
+
+            break;
+        }
+            default: { console.log(`Unknown command: ${input}`) }
+        }
+
+        rl.prompt();
+    }).on('close', () => 
+    {
+        console.log('Stopping bot...');
+        process.exit(0);
+    });
+}
+
+
+async function log(content)
+{
+    readline.clearLine(process.stdout, 0);       
+    readline.cursorTo(process.stdout, 0);        
+
+    console.log(content);                        
+
+    rl.prompt(true);                             
+}
+
+module.exports =
+{
+    log, devTools
+}
