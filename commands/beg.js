5f9d78c27ab694822ecd173e97cd0cdc0a2b56d4
LazyGran <xgraron@gmail.com>
2025-06-09 18:40:29 +0200

    Beg cooldown reduced to 5 minutes

--- commands/beg.js
+++ commands/beg.js
@@ -14,7 +14,7 @@ module.exports = {
     {
         await interaction.deferReply()
 
-        if((Date.now() - userStats.lastbeg) < 600000) return eh.error(interaction, "Give it a bit until you can beg again");
+        if((Date.now() - userStats.lastbeg) < 300000) return eh.error(interaction, "Give it a bit until you can beg again");
 
         const random    = new Random()
         const lucky     = random.integer(1, 100)
