c7e23fd19d0a2f9d9b5d2b5b4325f575418cdb86
LazyGran <xgraron@gmail.com>
2025-06-17 18:57:48 +0200

    Crime overhaul now has negative consequences

--- commands/crime.js
+++ commands/crime.js
@@ -16,17 +16,39 @@ module.exports = {
         if((Date.now() - userStats.lastcrime) < 300000) return eh.error(interaction, "Criminal... Chill out for a while");
 
         const random    = new Random()
-        const lucky     = random.integer(10, 100)
+        const lucky     = random.integer(1, 100)
 
-        let n               = random.integer(1, 100)
-        if(lucky === 7) n   = n*2;
-        
-        userStats.chips     = userStats.chips + n
-        userStats.lastcrime = Date.now()
+        let desc    = ""
+        let n       = random.integer(50, 500)
+
+        dev.log(userStats.chips)
+        dev.log(n) 
+
+        if     (lucky <= 34)            
+        {
+            desc                = `You commit a crime for some money, exchange it for **${n} Chips** \nUse it to gamble.`
+            userStats.chips     = userStats.chips + n
+            userStats.lastcrime = Date.now()
+        }
+
+        else if(userStats.chips < n)    
+        {
+            desc                = `You commit a crime but get caught. \nSince you're broke they just laugh and lets you go.`
+            userStats.lastcrime = Date.now()
+        }
+        else            
+        {
+            desc                = `You commit a crime for but get caught, they **take ${n} Chips** from you and let you go.`
+            n                   = n * (-1)
+            userStats.chips     = userStats.chips + n
+            userStats.lastcrime = Date.now()
+        }
+    
+        dev.log(userStats.chips)
 
         const embed = new EmbedBuilder()
         .setTitle(`Brokey...`)
-        .setDescription(`You commit a crime for some money, exchange it for ${n} chips \nUse it to gamble.`)
+        .setDescription(desc)
 
         await dh.userSave(userStats)
 
