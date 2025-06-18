6dc406703866b2d230548221587b4d8e0daf1c2c
LazyGran <xgraron@gmail.com>
2025-06-18 17:48:25 +0200

    Roulette multiplayer

--- games/roulette.js
+++ games/roulette.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } 	= require("discord.js")
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } 	= require("discord.js")
 const { Random }														= require("random-js")
 const cnvs  = require('@napi-rs/canvas');
 const dh 	= require("../handlers/dataHandler.js")
@@ -9,10 +9,22 @@ const dev   = require('../handlers/dev.js')
 async function main(interaction, bet, userStats, UID, chosen)
 {
 	const wheel 		= []
-	const players 		= []
+	const winners 		= []
+	const p_names		= []
 	const redNumbers 	= [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];	
+	const fields 		=  
+	[
+	    { name: "Red", value: "red"}, 
+	    { name: "Black", value: "black"},
+	    { name: "Even", value: "even"},
+	    { name: "Odd", value: "odd"},
+	    { name: "1st 12", value: "1st"},
+	    { name: "2nd 12", value: "2nd"},
+	    { name: "3rd 12", value: "3rd"},
+	    { name: "Zero", value: "green"},
+	]
 
-	let p_names 	= ""
+	let players = {}
 	let initial;
 
 	for(i = 0; i < 37; i++)
@@ -26,64 +38,82 @@ async function main(interaction, bet, userStats, UID, chosen)
 		wheel.push({ space: i, type: type })
 	}
 
-	players.push(interaction.user)
+	await addplayer(interaction.user, bet, chosen, players, p_names)
 
-	for(const u of players)
-	{
-		p_names += `${u.username}, `
-	}
+	const menu 	= new StringSelectMenuBuilder()
+	.setCustomId("Bet")
+	.setPlaceholder("Join this game of Roulette")
+	.addOptions(
+		...fields.map(field =>
+			new StringSelectMenuOptionBuilder()
+			.setLabel(field.name)
+			.setValue(field.value)
+		)
+	)
 
+	const row 	= new ActionRowBuilder().addComponents(menu)
 	const embed = new EmbedBuilder()
 	.setColor("#259dd9")
 	.setTitle("The wheel is spinning..")
 	.setThumbnail('https://media.discordapp.net/attachments/1242636042469642300/1376641025606549606/roulette_spin.gif?ex=68380a9d&is=6836b91d&hm=cb8533d5df401544391aa8aaa8e4e1c45ea70cea8b07d34d04c223cdf832c687&=')
-	.setDescription(`**Players:** \n${p_names}`)
-	.setFooter({ text: `Multiplayer coming soon..` });
+	.setDescription(`**Players:** \n${p_names.join(", ")} \n\nBet: ${bet}`)
 
-	try 	{ initial = await interaction.editReply({ embeds: [embed] }) }
+	try 	{ initial = await interaction.editReply({ embeds: [embed], components: [row] }) }
 	catch 	{ dev.log("Failed to respond \n GameID: 2, Error: 1", 2) }
 
-	setTimeout(async () => 
-	{ 
-		const field 			= await wheelspin(interaction, wheel, embed)
-		const { space, type }	= field
+	const selected	= await initial.createMessageComponentCollector({ time: 10_000 })
 
-		const won 		= await checkwin(chosen, space, type)
-		const xp_rew 	= Math.floor(bet / 7)
+	selected.on('collect', async selection =>
+	{
+		const s = dh.userGet(selection.user.id)
 
-		let reward = 0
-
-		if(["red", "black", "even", "odd"].includes(chosen))	reward = Math.floor(bet + (bet / 2));
-		else if(["1st", "2nd", "3rd"].includes(chosen))			reward = bet + bet;
-		else if(chosen === "green")								reward = bet * 10;
-
-		if(won)
+		if(s.chips < bet)
 		{
-			embed.setColor('#1aa32a').setDescription(`You won ${reward - bet}`)
-
-			userStats.chips 		= userStats.chips + reward
-			await xh.leveling(userStats, xp_rew)
+			selection.reply({ content: "You can't afford to join in", ephemeral: true})
+		}
+		else if(selection.user.id in players)
+		{
+			selection.reply({ content: "You already bet on this", ephemeral: true})
 		}
 		else
 		{
-			embed.setColor('#e80400').setDescription("You lost").setFooter({ text: `The house always wins...` })
+			//s.active_game 	= true
+			s.chips 		= s.chips - bet
+			chosen 			= selection.values[0]
+
+        	await dh.userSave(selection.user.id, s)	
+			await addplayer(selection.user, bet, chosen, players, p_names)
+
+			selection.deferUpdate()
+
+			embed.setDescription(`**Players:** \n${p_names.join(", ")} \n\nBet: ${bet}`)
+
+			try 	{ await interaction.editReply({ embeds: [embed] }) }
+			catch 	{ dev.log("Failed to respond \n GameID: 2, Error: 2", 2) }
 		}
+	})
+
+	selected.on('end', async collected =>
+	{
+		row.components = []
+
+		const field = await wheelspin(interaction, wheel, embed)
+
+		await getwinners(players, field, winners)
+
+		embed.setDescription(`**Winners:** \n${winners.join(", ")}`)
 
 		try 	{ await interaction.editReply({ embeds: [embed] }) }
-		catch 	{ dev.log("Failed to respond \n GameID: 2, Error: 2", 2) }
-
-		userStats.active_game = false
-
-		dh.userSave(UID, userStats)
-	}, 2000)
-
+		catch 	{ dev.log("Failed to respond \n GameID: 2, Error: 3", 2) }
+	})
 }
+
 
 async function wheelspin(interaction, wheel, embed)
 {
 	const random	= new Random()
 	const n 		= random.integer(0, 36)
-	const field 	= wheel[n]
+	const field = wheel[13]
     const canvas 	= cnvs.createCanvas(256, 256);
     const context 	= canvas.getContext('2d');
     const colors 	= 
@@ -111,33 +141,76 @@ async function wheelspin(interaction, wheel, embed)
 
 	embed 
 	.setColor("#259dd9")
-	.setTitle(`The ball landed on *${field.type} ${field.space}*`)
-	//.addFields({ name: 'Winners', value: "-# *Checking for winners*"}) //used for Multiplayer later
+	.setDescription(`**Winners:** \n-# *Checking for winners*`)
     .setThumbnail("attachment://image.png")
-	.setDescription(null)
-
 
 	try 	{ await interaction.editReply({embeds: [embed], files: [{attachment:b, name:'image.png'}] }) }
-	catch 	{ dev.log("Failed to respond \n GameID: 2, Error: 3", 2) }
+	catch 	{ dev.log("Failed to respond \n GameID: 2, Error: 4", 2) }
 
 	return field;
 }
 
-async function checkwin(chosen, space, type)
+async function getwinners(players, field, winners)
 {
-		switch(chosen)
+	for(const id in players)
+	{
+		const p = players[id]
+		const s = await dh.userGet(id)
+		const { space, type }	= field
+		const { name, bet, chosen }	= p
+
+		const won = await wincon(chosen, space, type)
+
+		if(won === true)
 		{
-			case "red": 	return type === chosen;
-			case "black": 	return type === chosen;
-			case "green": 	return type === chosen;
+			const xp_rew = Math.floor(bet / 7)
 
-			case "even": 	return space !== 0 && space % 2 === 0;
-			case "odd": 	return space !== 0 && space % 2 !== 0;
+			let reward = 0
 
-			case "1st": 	return space >= 1  && space <= 12;
-			case "2nd": 	return space >= 13 && space <= 24;
-			case "3rd": 	return space >= 25 && space <= 36;
+			if(["red", "black", "even", "odd"].includes(chosen))	reward = Math.floor(bet + (bet / 2));
+			else if(["1st", "2nd", "3rd"].includes(chosen))			reward = bet + bet;
+			else if(chosen === "green")								reward = bet * 10;
+
+			s.chips += reward
+
+			winners.push(name)
+
+			await xh.leveling(s, xp_rew)
 		}
+
+		s.active_game = false
+
+		await dh.userSave(id, s)
+	}
+}
+
+async function wincon(chosen, space, type)
+{
+	switch(chosen)
+	{
+		case "red": 	return type === chosen;
+		case "black": 	return type === chosen;
+		case "green": 	return type === chosen;
+
+		case "even": 	return space !== 0 && space % 2 === 0;
+		case "odd": 	return space !== 0 && space % 2 !== 0;
+
+		case "1st": 	return space >= 1  && space <= 12;
+		case "2nd": 	return space >= 13 && space <= 24;
+		case "3rd": 	return space >= 25 && space <= 36;
+	}
+}
+
+async function addplayer(p, bet, chosen, players, p_names)
+{
+	players[p.id] = 
+	{ 
+		name: p.username,
+		bet: bet,
+		chosen: chosen
+	}
+
+	p_names.push(p.username)
 }
 
 module.exports =
