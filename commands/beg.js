3583d7ac95ffecd0cb6be6ff1061930d04bffbdf
LazyGran <xgraron@gmail.com>
2025-06-19 22:48:47 +0200

    Added dynamic responses Beg ranges from 10-100 now (was 5-50)

--- commands/beg.js
+++ commands/beg.js
@@ -1,9 +1,24 @@
 const { SlashCommandBuilder, EmbedBuilder, } = require("discord.js")    
 const { Random }                             = require("random-js")
+const fs    = require('fs')
+const path  = require('path')
 const xh    = require('../handlers/xpHandler.js')
 const eh    = require('../handlers/errorHandler.js')    
 const dh    = require('../handlers/dataHandler.js')
 const dev   = require('../handlers/dev.js')
+
+const filePath      = path.join("database/", 'beg.txt')   
+const fileContent   = fs.readFileSync(filePath, 'utf-8')   
+const lines         = fileContent.split('\n')  
+const array         = []
+
+for(i = 0; i < lines.length; i += 2)
+{
+    const first     = lines[i]
+    const second    = lines[i + 1] 
+
+    array.push(`${first}\n${second}`)
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,8 +33,13 @@ module.exports = {
 
         const random    = new Random()
         const lucky     = random.integer(1, 10)
+        const r         = random.integer(0, array.length - 1)
+        const response  = array[r]
 
-        let n               = random.integer(5, 50)
+        dev.log(r)
+        dev.log(response)
+
+        let n               = random.integer(10, 100)
         if(lucky === 7) n   = n*2;
         
         userStats.chips     = userStats.chips + n
@@ -27,7 +47,7 @@ module.exports = {
 
         const embed = new EmbedBuilder()
         .setTitle(`Brokey...`)
-        .setDescription(`You beg for money and someone gives you ${n} chips \nUse it to gamble.`)
+        .setDescription(`${response} \n-# Gained **${n}** Chips`)
 
         await xh.leveling(userStats, 5)
         await dh.userSave(userStats)
