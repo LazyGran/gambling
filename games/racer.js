0a2a5a60109dd54910d3d5ee8170c8874b70ec14
LazyGran <xgraron@gmail.com>
2025-06-25 20:43:57 +0200

    button handling & proper timer creation

--- games/racer.js
+++ games/racer.js
@@ -6,15 +6,24 @@ const ch    = require('../handlers/cardHandler.js')
 const xh	= require('../handlers/xpHandler.js')
 const dev   = require('../handlers/dev.js')
 
-let obstacle;
-let lap;
+const random 	= new Random()
 
 async function main(interaction, bet, userStats, UID)
 {
-	let initial;
 	let round = 0
+	let initial;
+	let obstacle;
+	let race;
 
-	await game()
+	const length 	= random.integer(7, 13)
+	const close 	= Math.floor(Date.now() / 1000) + Math.floor(length + (length / 2))
+	const reaction 	= 1000 * Math.floor(length + (length / 2))
+
+	dev.log(reaction)
+
+	race = await lap()
+
+	dev.log(race)
 
 	const up = new ButtonBuilder()
 	.setCustomId("3")
@@ -35,27 +44,40 @@ async function main(interaction, bet, userStats, UID)
 	const embed = new EmbedBuilder()
 	.setColor("#259dd9")
 	.setTitle("Racer")
-	.setDescription(lap)
+	.setDescription(race + `\n-# Race ends <t:${close}:R>!`)
 
 	try 	{ initial = await interaction.editReply({ embeds: [embed], components: [row] }) }
 	catch 	{ dev.log("Failed to respond \n GameID: 6, Error: 1", 2) }
+
+	const pressed = await initial.createMessageComponentCollector({ time: reaction })
+
+	pressed.on('collect', async game =>
+	{
+		game.deferUpdate()
+
+		dev.log(game.customId)
+	})
+
+	pressed.on('end', async collected =>
+	{
+		dev.log("end")
+	})
 }
 
 async function lap()
 {
-	const random 	= new Random()
 	const obstacle  = random.integer(3, 5)
 
-	race = await track(obstacle)
+	return race = await track(obstacle)
 }
 
 async function track(n)
 {
 	switch(n)
 	{
-		case 3: 	return "```⬛     \n🔲     🏎️\n🔲     ```"
-		case 4: 	return "```🔲     \n⬛     🏎️\n🔲     ```"
-		case 5: 	return "```🔲     \n🔲     🏎️\n⬛     ```"
+		case 3: 	return "```⬛\n🔲     🏎️\n🔲```"
+		case 4: 	return "```🔲\n⬛     🏎️\n🔲```"
+		case 5: 	return "```🔲\n🔲     🏎️\n⬛```"
 	}
 }
 
