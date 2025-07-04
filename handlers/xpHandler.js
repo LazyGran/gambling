1e3ac3e0614fde6d4f9be9ac113b82e4d75ed1c8
LazyGran <xgraron@gmail.com>
2025-07-04 16:11:46 +0200

    Early version of achievement handler Successfully detects & adds all game-related base achievements

--- handlers/xpHandler.js
+++ handlers/xpHandler.js
@@ -1,6 +1,34 @@
 const jsonfile	= require("jsonfile")
 const fs		= require("fs")
 const dh 		= require("../handlers/dataHandler.js")
+const dev   	= require('../handlers/dev.js')
+
+const games_con = [
+	//Horl
+	{ gameID: 1, plays: 5, wins: 0, earnings: 0, achievementID: 101 },
+	{ gameID: 1, plays: 25, wins: 0, earnings: 0, achievementID: 102 },
+	{ gameID: 1, plays: 100, wins: 0, earnings: 0, achievementID: 103 },
+	//Roulette
+	{ gameID: 2, plays: 5, wins: 0, earnings: 0, achievementID: 201 },
+	{ gameID: 2, plays: 25, wins: 0, earnings: 0, achievementID: 202 },
+	{ gameID: 2, plays: 100, wins: 0, earnings: 0, achievementID: 203 },
+	//Seventeen
+	{ gameID: 3, plays: 5, wins: 0, earnings: 0, achievementID: 301 },
+	{ gameID: 3, plays: 25, wins: 0, earnings: 0, achievementID: 302 },
+	{ gameID: 3, plays: 100, wins: 0, earnings: 0, achievementID: 303 },
+	//Blackjack
+	{ gameID: 4, plays: 5, wins: 0, earnings: 0, achievementID: 401 },
+	{ gameID: 4, plays: 25, wins: 0, earnings: 0, achievementID: 402 },
+	{ gameID: 4, plays: 100, wins: 0, earnings: 0, achievementID: 403 },
+	//horse
+	{ gameID: 5, plays: 1, wins: 1, earnings: 0, achievementID: 501 },
+	//racer
+	{ gameID: 6, plays: 10, wins: 0, earnings: 0, achievementID: 601 },
+	//slots
+	{ gameID: 7, plays: 1, wins: 1, earnings: 10, achievementID: 701}
+]
+
+
 
 function leveling(userStats, reward)
 {
@@ -13,7 +41,42 @@ function leveling(userStats, reward)
 	dh.userSave(userStats)
 }
 
+function achievements(userStats, pre, won, gameID, reward)
+{
+	gameID = Number(gameID)
+	if(!userStats.games[gameID])
+	{
+		userStats.games[gameID] =
+		{
+			won: 0,
+			played: 0,
+			earned: 0
+		}
+	}
+
+	let achievement;
+
+	const game = userStats.games[gameID]
+
+	if(won) game.won ++;
+			game.played ++
+			game.earned += reward
+
+	dev.log(game)
+
+	for(const con of games_con)
+	{
+		if(gameID === con.gameID && game.played >= con.plays && game.won >= con.wins && game.earned >= con.earnings && !userStats.achievements.includes(con.achievementID))
+		{
+			dev.log("balls")
+			userStats.achievements.push(con.achievementID)
+		}
+	}
+
+	dh.userSave(userStats)
+}
+
 module.exports =
 {
-	leveling
+	leveling, achievements
 }
